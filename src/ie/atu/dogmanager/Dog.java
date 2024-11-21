b6a8f1e9c8aa771f50a8de651b35450bf2cb0ba4
G00428161 <145337544+G00428161@users.noreply.github.com>
2024-11-21 16:21:19 +0000

    Second Layout

--- src/ie/atu/dogmanager/Dog.java
+++ src/ie/atu/dogmanager/Dog.java
@@ -1,5 +1,43 @@
 package ie.atu.dogmanager;
 
 public class Dog {
+    // Instanve Variables //
+    private int microchipNumber; // Unique ID
+    private String name;
+    private float breed;
+    private boolean isHungry; // true if hungry, false if not//
+
+    // Getter and Setter Methods
+    public int getmicrochipNumber() {
+        return this.microchipNumber;
+    }
+
+    public String getname() {
+        return this.name;
+    }
+
+    public float getbreed() {
+        return this.breed;
+    }
+
+    public boolean getisHungry() {
+        return this.isHungry;
+    }
+
+    public void setMicrochipNumber(int microchipNumber) {
+        this.microchipNumber = microchipNumber;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public void setbreed(float breed) {
+        this.breed = breed;
+    }
+
+    public void setisHungry(boolean isHungry) {
+        this.isHungry = isHungry;
+    }
 
 }
